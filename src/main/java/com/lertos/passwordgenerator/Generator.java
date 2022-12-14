ed8e99bc1553a9269886f5aa0e930321a851dff4
Lertos <deetasticgaming@gmail.com>
2022-12-13 21:01:51 -0400

    Adding the random getter methods for each type of character

--- src/main/java/com/lertos/passwordgenerator/Generator.java
+++ src/main/java/com/lertos/passwordgenerator/Generator.java
@@ -1,8 +1,12 @@
 package com.lertos.passwordgenerator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Generator {
+
+    private List<Integer> symbolAsciiCharacters;
 
     private Random rng;
 
@@ -12,12 +16,29 @@ public class Generator {
     private boolean useSymbols;
 
     public Generator(boolean useLowercase, boolean useUppercase, boolean useNumbers, boolean useSymbols) {
+        this.symbolAsciiCharacters = createSymbolSet();
+
         this.rng = new Random();
 
         this.useLowercase = useLowercase;
         this.useUppercase = useUppercase;
         this.useNumbers = useNumbers;
         this.useSymbols = useSymbols;
+    }
+
+    private List<Integer> createSymbolSet() {
+        List<Integer> symbolList = new ArrayList<>();
+
+        for (int i=33; i<48; i++)
+            symbolList.add(i);
+        for (int i=58; i<65; i++)
+            symbolList.add(i);
+        for (int i=91; i<97; i++)
+            symbolList.add(i);
+        for (int i=123; i<127; i++)
+            symbolList.add(i);
+
+        return symbolList;
     }
 
     public void setUseLowercase(boolean useLowercase) {
@@ -47,9 +68,27 @@ public class Generator {
     }
 
     private char getRandomLetter(boolean isLower) {
-        int random = rng.nextInt(65,90);
+        int random = 0;
+
+        if (isLower)
+            rng.nextInt(97,123);
+        else
+            rng.nextInt(65,90);
 
         return (char) random;
+    }
+
+    private char getRandomNumber() {
+        int random = rng.nextInt(48,58);
+
+        return (char) random;
+    }
+
+    private char getRandomSymbol() {
+        int random = rng.nextInt(0, symbolAsciiCharacters.size());
+        int randomSymbolKey = symbolAsciiCharacters.get(random);
+
+        return (char) randomSymbolKey;
     }
 
 }
