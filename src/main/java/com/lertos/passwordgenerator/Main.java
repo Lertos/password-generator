cf5ae21453614ce6bce996ff5df6cfdb6f330ad1
Lertos <deetasticgaming@gmail.com>
2022-12-13 20:34:27 -0400

    Removing test outputs

--- src/main/java/com/lertos/passwordgenerator/Main.java
+++ src/main/java/com/lertos/passwordgenerator/Main.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("java version: "+System.getProperty("java.version"));
-        System.out.println("javafx.version: " + System.getProperty("javafx.version"));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
