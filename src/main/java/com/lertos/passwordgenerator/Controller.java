b3231420567e59feef4cb8829589a17ab8a55ff3
Lertos <deetasticgaming@gmail.com>
2022-12-15 18:39:46 -0400

    Adding in the logic to copy the password to your clipboard

--- src/main/java/com/lertos/passwordgenerator/Controller.java
+++ src/main/java/com/lertos/passwordgenerator/Controller.java
@@ -8,10 +8,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
 public class Controller {
     @FXML
     private Button randomizeButton;
+    @FXML
+    private Button copyButton;
     @FXML
     private CheckBox cbLowercase;
     @FXML
@@ -61,6 +65,11 @@ public class Controller {
             regeneratePassword();
         }
         //Copy button
+        else if (e.getSource().equals(copyButton)) {
+            ClipboardContent content = new ClipboardContent();
+            content.putString(labelPassword.getText());
+            Clipboard.getSystemClipboard().setContent(content);
+        }
     }
 
     @FXML
