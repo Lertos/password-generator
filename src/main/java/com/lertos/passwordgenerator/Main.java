632d700efb8e814daf6002c1aa23d5696a769d2d
Lertos <deetasticgaming@gmail.com>
2022-12-15 18:34:46 -0400

    Changing the title of the application from Hello!

--- src/main/java/com/lertos/passwordgenerator/Main.java
+++ src/main/java/com/lertos/passwordgenerator/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Password Generator V1.0");
         stage.setScene(scene);
         stage.show();
     }
